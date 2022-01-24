75cc01e5ddcd764d3a70e736d1e15e6fb71c4770
tvhahn <tvhahn@gmail.com>
2022-01-24 13:10:37 -0800

    tested setup.py and works

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # PyPHM
 Datasets specific to Prognostics and Health Management. Easily download and prepare the data before feature engineering or model training.
+
+Once `setup.py` is established, run `python -m pip install -e .` to install the package on your local machine.
+
+
--- pyphm/datasets/utils.py
+++ pyphm/datasets/utils.py
@@ -17,7 +17,7 @@ from typing import Any, Callable, List, Iterable, Optional, TypeVar, Dict, IO, T
 from urllib.parse import urlparse
 
 import requests
-import torch
+# import torch
 from tqdm.auto import tqdm
 
 
@@ -445,29 +445,29 @@ def iterable_to_str(iterable: Iterable) -> str:
 T = TypeVar("T", str, bytes)
 
 
-def verify_str_arg(
-    value: T,
-    arg: Optional[str] = None,
-    valid_values: Iterable[T] = None,
-    custom_msg: Optional[str] = None,
-) -> T:
-    if not isinstance(value, torch._six.string_classes):
-        if arg is None:
-            msg = "Expected type str, but got type {type}."
-        else:
-            msg = "Expected type str for argument {arg}, but got type {type}."
-        msg = msg.format(type=type(value), arg=arg)
-        raise ValueError(msg)
+# def verify_str_arg(
+#     value: T,
+#     arg: Optional[str] = None,
+#     valid_values: Iterable[T] = None,
+#     custom_msg: Optional[str] = None,
+# ) -> T:
+#     if not isinstance(value, torch._six.string_classes):
+#         if arg is None:
+#             msg = "Expected type str, but got type {type}."
+#         else:
+#             msg = "Expected type str for argument {arg}, but got type {type}."
+#         msg = msg.format(type=type(value), arg=arg)
+#         raise ValueError(msg)
 
-    if valid_values is None:
-        return value
+#     if valid_values is None:
+#         return value
 
-    if value not in valid_values:
-        if custom_msg is not None:
-            msg = custom_msg
-        else:
-            msg = "Unknown value '{value}' for argument {arg}. Valid values are {{{valid_values}}}."
-            msg = msg.format(value=value, arg=arg, valid_values=iterable_to_str(valid_values))
-        raise ValueError(msg)
+#     if value not in valid_values:
+#         if custom_msg is not None:
+#             msg = custom_msg
+#         else:
+#             msg = "Unknown value '{value}' for argument {arg}. Valid values are {{{valid_values}}}."
+#             msg = msg.format(value=value, arg=arg, valid_values=iterable_to_str(valid_values))
+#         raise ValueError(msg)
 
-    return value
+#     return value
