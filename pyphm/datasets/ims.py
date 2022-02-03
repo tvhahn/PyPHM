273d28ec472686ebc7655f5d164663685ec0aea3
tvhahn <tvhahn@gmail.com>
2022-02-03 14:04:07 -0800

    Implement multiprocessing in load_run_as_df function.

--- pyphm/datasets/ims.py
+++ pyphm/datasets/ims.py
@@ -1,8 +1,10 @@
+from ossaudiodev import control_names
 import scipy.io as sio
 import numpy as np
 import pandas as pd
 from pathlib import Path
 from .pyphm import PHMDataset
+import multiprocessing as mp
 from typing import Any, Callable, List, Optional, Tuple
 from .utils import (
     download_and_extract_archive,
@@ -140,29 +142,39 @@ class ImsDataLoad(PHMDataset):
             extract_archive(self.dataset_path / rar_file, remove_finished=True)
 
     @staticmethod
-    def process_raw_csv(
-        path_run_folder: Path,
-        file_name: str, # the name of the .csv file
-        sample_index: int,
-        run_no: int,
-        sample_freq: float = 20480.0,
-    ) -> None:
+    def process_raw_csv(file_info_dict) -> None:
         """Load an individual sample (.csv file) of the IMS data set."""
+
+        path_run_folder = file_info_dict["path_run_folder"]
+        file_name = file_info_dict["file_name"]
+        sample_freq = file_info_dict["sample_freq"]
+        col_names = file_info_dict["col_names"]
+        run_no = file_info_dict["run_no"]
+        sample_index = file_info_dict["sample_index"]
 
         # load the .csv file
         signals_array = np.loadtxt(path_run_folder / file_name, delimiter="\t")
 
-        id_list = [f'{run_no}_{sample_index}'] * len(signals_array)
+        id_list = [f"{run_no}_{sample_index}"] * len(signals_array)
         run_list = [run_no] * len(signals_array)
         file_list = [file_name] * len(signals_array)
-        time_step_array = np.linspace(0.0, len(signals_array) / sample_freq, len(signals_array))
+        time_step_array = np.linspace(
+            0.0, len(signals_array) / sample_freq, len(signals_array)
+        )
 
-        return signals_array, id_list, run_list, file_list, time_step_array
+        df = pd.DataFrame(np.vstack(signals_array), columns=col_names)
+        df["id"] = id_list
+        df["run"] = run_list
+        df["file"] = file_list
+        df["time_step"] = np.hstack(time_step_array)
 
+        return df
 
-    def load_run_as_df(self, 
+    def load_run_as_df(
+        self,
         run_no: int,
-        ) -> None:
+        n_jobs: int = None,
+    ) -> None:
         """Load the three runs as individual dataframes."""
 
         if run_no == 1:
@@ -175,40 +187,37 @@ class ImsDataLoad(PHMDataset):
             col_names = self.col_3rd_names
             path_run_folder = self.path_3rd_folder
 
-        # get list of every file in the folder and sort by ascending date       
+        # get list of every file in the folder and sort by ascending date
         file_list = sorted(os.listdir(path_run_folder))
         print("len file_list:", len(file_list))
 
-        # instantiate the empty lists
-        signals = []
-        ids = []
-        runs = []
-        files = []
-        time_steps = []
+        # create a list of dictionaries containing the metadata for each file
+        file_info_list = []
+        for i, file_name in enumerate(sorted(os.listdir(path_run_folder))):
+            file_info_list.append(
+                {
+                    "path_run_folder": path_run_folder,
+                    "file_name": file_name,
+                    "sample_freq": self.sample_freq,
+                    "col_names": col_names,
+                    "run_no": run_no,
+                    "sample_index": i,
+                }
+            )
 
-        for i, file_name in enumerate(file_list):
-            if i % 50 == 0:
-                print(f"Processing {file_name}, i={i} - run {run_no}")
-            (
-            signals_array, 
-            id_list, 
-            run_list, 
-            file_list, 
-            time_step_array) = self.process_raw_csv(path_run_folder, file_name, i, run_no, sample_freq=20480.0)
+        # get number of cpu cores
+        if n_jobs is None:
+            n_jobs = mp.cpu_count() - 2
+        if n_jobs < 1:
+            n_jobs = 1
 
-            # append or extend to the lists
-            signals.append(signals_array)
-            ids.extend(id_list)
-            runs.extend(run_list)
-            files.extend(file_list)
-            time_steps.append(time_step_array)
+        print("n_jobs:", n_jobs)
 
-        # create the dataframe, starting with the signals
-        df = pd.DataFrame(np.vstack(signals), columns=col_names)
-        df["id"] = ids
-        df["run"] = runs
-        df["file"] = files
-        df["time_step"] = np.hstack(time_steps)
+        with mp.Pool(processes=n_jobs) as pool:
+
+            # from https://stackoverflow.com/a/36590187
+            df_run = pool.map(self.process_raw_csv, file_info_list)
+            df = pd.concat(df_run, ignore_index=True)
 
         return df
 
