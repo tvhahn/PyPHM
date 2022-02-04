9a1486034a46592494e700a319c66cb9933b09c3
tvhahn <tvhahn@gmail.com>
2022-02-04 13:42:22 -0800

    Integration test for milling dataset complete.

--- tests/integration/test_integration_milling.py
+++ tests/integration/test_integration_milling.py
@@ -9,20 +9,20 @@ from pyphm.datasets.milling import MillingPrepMethodA
 class TestMilling(unittest.TestCase):
     def setUp(self):
         # path to mill_truncated.mat
-        self.path_data_raw_folder = (
-            Path(__file__).parent / "fixtures/milling/mill_truncated.mat"
+        self.root = (
+            Path(__file__).parent / "fixtures"
         )
-        print("mill_data_path:", self.mill_data_path)
+        print("mill_data_path:", self.root)
 
         # path to milling_labels_with_tool_class_truncated.csv
         self.labels_path = (
-            Path(__file__).parent
-            / "fixtures/milling/milling_labels_with_tool_class_truncated.csv"
+            self.root
+            / "milling/milling_labels_with_tool_class_truncated.csv"
         )
 
         # path to milling_truncated_results.csv.gz
         self.results_path = (
-            Path(__file__).parent / "fixtures/milling/milling_truncated_results.csv.gz"
+            self.root / "milling/milling_truncated_results.csv.gz"
         )
 
     def test_milling_data_prep(self):
@@ -30,18 +30,16 @@ class TestMilling(unittest.TestCase):
 
         # load the data and instantiate the data prep class
         mill = MillingPrepMethodA(
-            self.path_data_raw_folder,
+            self.root,
             window_size=64,
             stride=64,
-            cut_drop_list=[17, 94],
+            cut_drop_list=[],
             path_df_labels=self.labels_path,
             download=False,
         )
 
         # create the results dataframe
         df = mill.create_xy_dataframe()
-        print("df.shape:", df.shape)
-        print("df.columns:", df.columns)
 
         # load the ground truth results dataframe
         col_names_ordered = [
@@ -79,9 +77,6 @@ class TestMilling(unittest.TestCase):
             self.results_path,
             compression="gzip",
         ).astype(col_dtype_dict)
-
-        print("df_gt.shape:", df_gt.shape)
-        print("df_gt.columns:", df_gt.columns)
 
         # compare the results
         assert_frame_equal(df, df_gt)
