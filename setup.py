9c4f08cea15c7e8dd44178c4fa15a49f8a42221f
tvhahn <tvhahn@gmail.com>
2022-02-17 15:02:59 -0700

    Work on packaging

--- setup.py
+++ setup.py
@@ -1,7 +1,12 @@
-from setuptools import setup, find_packages
+import setuptools
 
-setup(
-    name="pyphm",
-    version="0.1.0",
-    packages=find_packages(),
-)
+if __name__ == "__main__":
+    setuptools.setup()
+
+# from setuptools import setup, find_packages
+
+# setup(
+#     name="pyphm",
+#     version="0.1.0",
+#     packages=find_packages(),
+# )
